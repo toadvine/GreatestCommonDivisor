d3e58609c086d9e2e6ef96894b1aff5a57f2f064
toadvine <drubbles@gmail.com>
2015-08-28 17:35:46 -0700

    Trying out how commit works in PyCharm tsadg

--- GCD.py
+++ GCD.py
@@ -1,8 +1,17 @@
 __author__ = 'benjamin_sanchez'
 
 #find some divisors
+def main():
+    aCount = int(input())
+    for x in range(aCount):
+        bNums = [int(x) for x in input().split()]
+        print (GCD(bNums), end=" ")
+        print (LCM(bNums))
 
-aCount = int(input())
-for x in range(aCount):
-    bNums = [int(x) for x in input().split()]
+def GCD()
+    notComplete = True
+    while notComplete:
+
+
+
 
